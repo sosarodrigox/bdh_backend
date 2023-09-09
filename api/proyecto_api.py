29dea52243016b700066c5dcde0e67b3596116e0
sosarodrigox <sosarodrigox@gmail.com>
2023-09-09 19:43:32 -0300

    ultimo

--- api/proyecto_api.py
+++ api/proyecto_api.py
@@ -5,8 +5,7 @@ from models.proyectos.proyectos_api import ProyectoSinId, Proyecto, ProyectoList
 from repository.proyectos_repository import ProyectosRepository
 
 # Router:
-proyectos_api = APIRouter(
-    prefix='/proyectos', tags=['Proyectos'])
+proyectos_api = APIRouter(prefix="/proyectos", tags=["Proyectos"])
 
 # Repository:
 proyecto_repository = ProyectosRepository()
@@ -14,7 +13,7 @@ proyecto_repository = ProyectosRepository()
 # Endpoints:
 
 
-@proyectos_api.get('', response_model=List[ProyectoList])
+@proyectos_api.get("", response_model=List[ProyectoList])
 def get_all(db=Depends(get_db)):
     return proyecto_repository.get_all(db)
 
@@ -34,15 +33,19 @@ def get_all(db=Depends(get_db)):
 #     return proyecto_repository.get_cooperativas(db)
 
 
-@proyectos_api.get('/{id}', response_model=ProyectoList)
+@proyectos_api.get("/{id}", response_model=ProyectoList)
 def get_by_id(id: int, db=Depends(get_db)):
     proyecto = proyecto_repository.get_by_id(id, db)
     if proyecto is None:
-        raise HTTPException(
-            status_code=404, detail="Proyecto no encontrado")
+        raise HTTPException(status_code=404, detail="Proyecto no encontrado")
     return proyecto
 
 
-@proyectos_api.post('', response_model=ProyectoList)
+@proyectos_api.post("", response_model=ProyectoList)
 def create(proyecto: ProyectoSinId, db=Depends(get_db)):
     return proyecto_repository.create(proyecto, db)
+
+
+@proyectos_api.put("/{id}", response_model=ProyectoList)
+def update(id: int, proyecto: ProyectoSinId, db=Depends(get_db)):
+    return proyecto_repository.update(id, proyecto, db)
--- repository/proyectos_repository.py
+++ repository/proyectos_repository.py
@@ -26,3 +26,12 @@ class ProyectosRepository():
         db.add(nuevo_proyecto_bd)
         db.commit()
         return nuevo_proyecto_bd
+
+    def update(self, id: int, up: ProyectoSinId, db: Session):
+        proyecto_bd: ProyectoBd = db.execute(
+            select(ProyectoBd).filter(ProyectoBd.id == id)).scalars().first()
+        proyecto_bd.nombre = up.nombre
+        proyecto_bd.tipo_up = up.tipo_up
+        proyecto_bd.persona_id = up.persona_id
+        db.commit()
+        return proyecto_bd
